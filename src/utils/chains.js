ca46c27d6bfcd7ddadf68561590795805fc11812
rahul-rathore786 <rahulrathorese786@gmail.com>
2025-07-20 22:26:03 +0530

    env rpc url in reat app

--- src/utils/chains.js
+++ src/utils/chains.js
@@ -3,12 +3,10 @@ export const SUPPORTED_CHAINS = {
   sepolia: {
     name: "Sepolia (ETH Testnet)",
     chainId: 11155111,
-    // Using Infura endpoint with API key to avoid CORS issues
-    // You should replace 'YOUR_INFURA_API_KEY' with an actual API key from https://infura.io
-    // Free tier is available
     // public rpc url
     publicRpcUrl: "https://sepolia.publicnode.com", // This is a public key for testing
-    rpcUrl: "https://sepolia.infura.io/v3/e5429ea343fd490295f1cd7c036c30d9", // This is a public key for testing
+    // use .env file to store the rpc url
+    rpcUrl: process.env.REACT_APP_SEPOLIA_RPC_URL, // This is a public key for testing
     faucetAddress: "0x0DDE3Ef02DACAB3ce033095156a28c777622D85b",
     tokenAddress: "0xf7d57A3105030D3A9A525a169DCBE05a92Eeb382",
     tokenSymbol: "mUSDT",
@@ -54,7 +52,8 @@ export const SUPPORTED_CHAINS = {
     name: "Hyperion",
     chainId: 133717,
     publicRpcUrl: "https://hyperion-testnet.metisdevops.link",
-    rpcUrl: "https://hyperion-testnet.metisdevops.link",
+    // use .env file to store the rpc url
+    rpcUrl: process.env.REACT_APP_HYPERION_RPC_URL, // This is a public key for testing
     faucetAddress: "0xe6219F53844B8aD16720663bE1d8724D329f45F3", // Replace with actual faucet contract address
     tokenAddress: "0x5c7FbBF922643eaea24F4Cf7FD2F220e70659Ce7", // Replace with actual token address
     tokenSymbol: "mUSDT",
