97d9e01c907cafe2028fbbd510547404574712ca
rahul-rathore786 <rahulrathorese786@gmail.com>
2025-07-22 14:33:32 +0530

    chain config

--- src/utils/chains.js
+++ src/utils/chains.js
@@ -1,53 +1,5 @@
 // Configuration for supported blockchains and their respective tokens
 export const SUPPORTED_CHAINS = {
-  sepolia: {
-    name: "Sepolia (ETH Testnet)",
-    chainId: 11155111,
-    // public rpc url
-    publicRpcUrl: "https://sepolia.publicnode.com", // This is a public key for testing
-    // use .env file to store the rpc url
-    rpcUrl: process.env.REACT_APP_SEPOLIA_RPC_URL, // This is a public key for testing
-    faucetAddress: "0x0DDE3Ef02DACAB3ce033095156a28c777622D85b",
-    tokenAddress: "0xf7d57A3105030D3A9A525a169DCBE05a92Eeb382",
-    tokenSymbol: "mUSDT",
-    tokenDecimals: 18,
-    explorer: "https://sepolia.etherscan.io",
-    tokenImage: "https://cryptologos.cc/logos/tether-usdt-logo.png",
-  },
-  // More chains can be added here as needed
-  // polygon_mumbai: {
-  //   name: "Mumbai (Polygon Testnet)",
-  //   chainId: 80001,
-  //   rpcUrl: "https://rpc-mumbai.maticvigil.com",
-  //   faucetAddress: "0xYourMumbaiFaucetAddress", // Replace with actual faucet contract address
-  //   tokenAddress: "0xYourMumbaiTokenAddress", // Replace with actual token address
-  //   tokenSymbol: "TESTTKN",
-  //   tokenDecimals: 18,
-  //   explorer: "https://mumbai.polygonscan.com",
-  //   tokenImage: "",
-  // },
-  bnb_testnet: {
-    name: "BSC Testnet",
-    chainId: 97,
-    rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545",
-    faucetAddress: "0xYourBSCFaucetAddress", // Replace with actual faucet contract address
-    tokenAddress: "0xYourBSCTokenAddress", // Replace with actual token address
-    tokenSymbol: "BSCTKN",
-    tokenDecimals: 18,
-    explorer: "https://testnet.bscscan.com",
-    tokenImage: "",
-  },
-  // arbitrum_goerli: {
-  //   name: "Arbitrum Goerli",
-  //   chainId: 421613,
-  //   rpcUrl: "https://goerli-rollup.arbitrum.io/rpc",
-  //   faucetAddress: "0xYourArbitrumFaucetAddress", // Replace with actual faucet contract address
-  //   tokenAddress: "0xYourArbitrumTokenAddress", // Replace with actual token address
-  //   tokenSymbol: "ARBTKN",
-  //   tokenDecimals: 18,
-  //   explorer: "https://goerli.arbiscan.io",
-  //   tokenImage: "",
-  // },
   hyperion: {
     name: "Hyperion",
     chainId: 133717,
@@ -62,4 +14,54 @@ export const SUPPORTED_CHAINS = {
     tokenImage:
       "https://assets.coingecko.com/coins/images/35001/standard/logo.png?1706959346",
   },
+  sepolia: {
+    name: "Sepolia (ETH Testnet)",
+    chainId: 11155111,
+    // public rpc url
+    publicRpcUrl: "https://sepolia.publicnode.com", // This is a public key for testing
+    // use .env file to store the rpc url
+    rpcUrl: process.env.REACT_APP_SEPOLIA_RPC_URL, // This is a public key for testing
+    faucetAddress: "0x0DDE3Ef02DACAB3ce033095156a28c777622D85b",
+    tokenAddress: "0xf7d57A3105030D3A9A525a169DCBE05a92Eeb382",
+    tokenSymbol: "mUSDT",
+    tokenDecimals: 18,
+    explorer: "https://sepolia.etherscan.io",
+    tokenImage: "https://cryptologos.cc/logos/tether-usdt-logo.png",
+  },
+
+  bnb_testnet: {
+    name: "BSC Testnet",
+    chainId: 97,
+    rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545",
+    faucetAddress: "0xYourBSCFaucetAddress", // Replace with actual faucet contract address
+    tokenAddress: "0xYourBSCTokenAddress", // Replace with actual token address
+    tokenSymbol: "bUSDT",
+    tokenDecimals: 18,
+    explorer: "https://testnet.bscscan.com",
+    tokenImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCcTZRsvk_J_iJ0wdZVBEHFKnZTV0oZLhrvw&s",
+  },
+  // arbitrum_goerli: {
+  //   name: "Arbitrum Goerli",
+  //   chainId: 421613,
+  //   rpcUrl: "https://goerli-rollup.arbitrum.io/rpc",
+  //   faucetAddress: "0xYourArbitrumFaucetAddress", // Replace with actual faucet contract address
+  //   tokenAddress: "0xYourArbitrumTokenAddress", // Replace with actual token address
+  //   tokenSymbol: "ARBTKN",
+  //   tokenDecimals: 18,
+  //   explorer: "https://goerli.arbiscan.io",
+  //   tokenImage: "",
+  // },
+  // More chains can be added here as needed
+  // polygon_mumbai: {
+  //   name: "Mumbai (Polygon Testnet)",
+  //   chainId: 80001,
+  //   rpcUrl: "https://rpc-mumbai.maticvigil.com",
+  //   faucetAddress: "0xYourMumbaiFaucetAddress", // Replace with actual faucet contract address
+  //   tokenAddress: "0xYourMumbaiTokenAddress", // Replace with actual token address
+  //   tokenSymbol: "TESTTKN",
+  //   tokenDecimals: 18,
+  //   explorer: "https://mumbai.polygonscan.com",
+  //   tokenImage: "",
+  // },
 };
